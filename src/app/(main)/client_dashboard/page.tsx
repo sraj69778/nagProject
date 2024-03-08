79317bb79d8492581b0abeda414ea99b7397d917
Ayush Kamboj <aayush.kamboj@stackera.io>
2024-03-09 01:28:26 +0530

    Content tile added in the client dashboard

--- src/app/(main)/client_dashboard/page.tsx
+++ src/app/(main)/client_dashboard/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import Messages from "../page-components/Messages";
 import Requests from "../page-components/Requests";
+import ContentTile from "../page-components/content-tile";
 
 const Dashboard = () => {
   return (
     <>
       <div className="w-[80%]">
-        <div className="m-[1rem] ml-0 px-[2%] py-[1%] h-[50vh] rounded-xl bg-[#fff]">
+        <div className="m-[1rem] ml-0 px-[2%]  h-[50vh] rounded-xl bg-[#fff] ">
           <div className="flex items-center justify-between mb-[0.3rem]">
             <h2 className="text-[#717171] text-[28px] font-bold">Dashboard</h2>
             <Image
@@ -19,10 +20,18 @@ const Dashboard = () => {
             ></Image>
           </div>
           <Separator className="bg-[#d7d7d7] mx-auto h-[0.2rem] text-center " />
+          <div className="flex flex-wrap justify-between">
+            <ContentTile />
+            <ContentTile />
+            <ContentTile />
+            <ContentTile />
+            <ContentTile />
+            <ContentTile />
+          </div>
         </div>
         <div className="flex">
           <Messages />
-          <Requests />  
+          <Requests />
         </div>
       </div>
     </>
