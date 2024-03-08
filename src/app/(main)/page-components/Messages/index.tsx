eea18a9b2c32be6dc8a36dea27b67c87c72e264e
Ayush Kamboj <aayush.kamboj@stackera.io>
2024-03-09 02:41:53 +0530

    Content Tile added in the Messages and Request components

--- src/app/(main)/page-components/Messages/index.tsx
+++ src/app/(main)/page-components/Messages/index.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
+import ContentTile from "../content-tile";
 
 const Messages = () => {
   return (
     <>
-      <div className="m-[1rem] ml-0 mt-0 px-[1.5rem] py-[1rem] h-[43.5vh] rounded-xl w-[50%] bg-[#fff]">
+      <div className="m-[1rem] ml-0 mt-0 px-[1.5rem] py-[1rem] h-[47vh] rounded-xl w-[50%] bg-[#fff]">
         <div className="flex items-center justify-between mb-[0.3rem]">
           <h2 className="text-[#717171] text-[28px] font-bold">Messages</h2>
           <Image
-              className="cursor-pointer"
-              src={"/dashboard-vendor/expand.svg"}
-              width={20}
-              height={90}
-              alt="Project Logo"
-            ></Image>
+            className="cursor-pointer"
+            src={"/dashboard-vendor/expand.svg"}
+            width={20}
+            height={90}
+            alt="Project Logo"
+          ></Image>
         </div>
         <Separator className="bg-[#d7d7d7] mx-auto h-[0.2rem] text-center " />
+        <div>
+          <ContentTile />
+          <ContentTile />
+          <ContentTile />
+        </div>
       </div>
     </>
   );
--- src/app/(main)/page-components/Requests/index.tsx
+++ src/app/(main)/page-components/Requests/index.tsx
@@ -1,10 +1,12 @@
 import { Separator } from "@/components/ui/separator";
+import ContentTile from "../content-tile";
 import Image from "next/image";
+
 const Requests = () => {
   return (
     <>
       <>
-        <div className="m-[1rem] ml-0 mt-0 px-[1.5rem] py-[1rem] h-[43.5vh] rounded-xl w-[50%] bg-[#fff]">
+        <div className="m-[1rem] ml-0 mt-0 px-[1.5rem] py-[1rem] h-[47vh] rounded-xl w-[50%] bg-[#fff]">
           <div className="flex items-center justify-between mb-[0.3rem]">
             <h2 className="text-[#717171] text-[28px] font-bold">Requests</h2>
             <Image
@@ -16,6 +18,11 @@ const Requests = () => {
             ></Image>
           </div>
           <Separator className="bg-[#d7d7d7] mx-auto h-[0.2rem] text-center " />
+          <div>
+            <ContentTile />
+            <ContentTile />
+            <ContentTile />
+          </div>
         </div>
       </>
     </>
