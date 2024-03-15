acddda32ef1dd7acaee2824db441684416d0df16
Ayush Kamboj <aayush.kamboj@stackera.io>
2024-03-16 04:23:12 +0530

    Adding useEffect to redirect index page to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/login");
+  useEffect(() => {
+    router.push("/login");
+  }, []);
   return <></>;
 }
