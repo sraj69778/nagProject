f00366f659e29fd5a7e9867544f15f7655edd490
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-21 00:22:59 +0530

    Dynamic Selection added in the side nav

--- src/app/(main)/page-components/Side-Nav/index.tsx
+++ src/app/(main)/page-components/Side-Nav/index.tsx
@@ -1,45 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
-var options = [
-  {
-    name : 'Home',
-    image: '/dashboard-vendor/home.svg',
-    isSelected: false,
+import { usePathname, useRouter } from "next/navigation";
 
- },
- {
-  name : 'Dashboard',
-  image: '/dashboard-vendor/dashboard.svg',
-  isSelected: false,
-
-},
-{
-  name : 'Settings',
-  image: '/dashboard-vendor/settings.svg',
-  isSelected: true,
-
-},
-{
-  name : 'Chats',
-  image: '/dashboard-vendor/chat.svg',
-  isSelected: false,
-
-},
-{
-  name : 'Requests',
-  image: '/dashboard-vendor/requests.svg',
-  isSelected: false,
-
-},
-{
-  name : 'Profile',
-  image: '/dashboard-vendor/profile.svg',
-  isSelected: false,
-
-},
-]
 const SideNav = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  let userData: any = sessionStorage.getItem("userData");
+  userData = JSON.parse(userData);
+
+  let options = [
+    {
+      name: "Dashboard",
+      image: "/dashboard-vendor/dashboard.svg",
+      isSelected: pathname.includes("dashboard"),
+      url: `${userData.user.role.toLowerCase()}_dashboard`,
+    },
+    {
+      name: "Chats",
+      image: "/dashboard-vendor/chat.svg",
+      isSelected: pathname.includes("chat"),
+      url: "/chats",
+    },
+    {
+      name: "Requests",
+      image: "/dashboard-vendor/requests.svg",
+      isSelected: pathname.includes("requests"),
+      url: "/requests",
+    },
+    {
+      name: "Profile",
+      image: "/dashboard-vendor/profile.svg",
+      isSelected: pathname.includes("profile"),
+      url: "/profile",
+    },
+  ];
   return (
     <>
       <div className="bg-[#fff] w-[18%] h-[96.5vh] m-[1%] rounded-3xl mr-[1%] py-[1%]">
@@ -56,19 +53,29 @@ const SideNav = () => {
         <Separator className="bg-[#d7d7d7] w-[90%] mx-auto h-[0.2rem] text-center " />
         {/* Side Nav Buttons */}
         <div>
-          {options.map((option)=>(
-             <div key = {option.name} className={` ${option.isSelected? 'bg-[#1A88E1] text-[#ffffff]':'text-[#676767]'}   shadow-lg cursor-pointer mx-[8%] my-[4%] p-[3%] rounded-2xl flex items-center`}>
-             <Image
-               src={option.image}
-               width={30}
-               height={100}
-               alt={`${option.name} Logo`}
-             ></Image>
-             <Button className="lg:text-[16px] md:text-[12px] font-medium">{option.name}</Button>
-           </div>
+          {options.map((option) => (
+            <div
+              onClick={() => {
+                router.push(option.url);
+              }}
+              key={option.name}
+              className={` ${
+                option.isSelected
+                  ? "bg-[#1A88E1] text-[#ffffff]"
+                  : "text-[#676767]"
+              }   shadow-lg cursor-pointer mx-[8%] my-[4%] p-[3%] rounded-2xl flex items-center`}
+            >
+              <Image
+                src={option.image}
+                width={30}
+                height={100}
+                alt={`${option.name} Logo`}
+              ></Image>
+              <Button className="lg:text-[16px] md:text-[12px] font-medium">
+                {option.name}
+              </Button>
+            </div>
           ))}
-         
-          
         </div>
       </div>
     </>
