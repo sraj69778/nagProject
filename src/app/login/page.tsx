2cae0a6d9d25b6b46af1b216a1051acdbabe007d
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-20 17:40:15 +0530

    Login api integrated using TanStack Query

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 const Login = () => {
+  const { mutate, isError, isSuccess, isPending, data }: any = useMutation({
+    mutationFn: (user) => {
+      return axios
+        .post("http://localhost:3000/user/login", user)
+        .then((res) => res.data);
+    },
+  });
+
+  // console.log(data);
+
   return (
     <>
-      <div className="bg-[#181B1C] h-[100vh] flex">
+      <div className="bg-[#181B1C] min-h-[100vh] flex">
         {/* Login Section */}
         <div className="w-[60%]">
           <div className="flex items-center px-[1rem]">
@@ -34,27 +48,46 @@ const Login = () => {
 
             {/* Form Section */}
             <div className="w-[65%] mx-auto">
-              <div>
-                <Label className="text-[#D9D9D9]" htmlFor="email">
-                  Email*
-                </Label>
-                <Input className="bg-[#D9D9D9] mt-[5px] rounded" type="email" />
-              </div>
-              <div className="my-[1rem]">
-                <Label className="text-[#D9D9D9]" htmlFor="email">
-                  Password*
-                </Label>
-                <Input
-                  className="bg-[#D9D9D9] mt-[5px] rounded"
-                  type="password"
-                />
-              </div>
-              <Button
-                type="submit"
-                className="py-[1.5rem] hover:bg-blue-600 font-bold text-[#ffffff] bg-[#1A88E1] w-full my-[6%] rounded"
+              <form
+                onSubmit={(event: any) => {
+                  event.preventDefault();
+                  mutate({
+                    email: event.target.email.value,
+                    password: event.target.password.value,
+                  });
+                  // console.log(event.target.email.value);
+                  // console.log(event.target.password.value);
+                }}
               >
-                LOGIN IN
-              </Button>
+                <div>
+                  <Label className="text-[#D9D9D9]" htmlFor="email">
+                    Email*
+                  </Label>
+                  <Input
+                    required
+                    className="bg-[#D9D9D9] mt-[5px] rounded"
+                    type="email"
+                    name="email"
+                  />
+                </div>
+                <div className="my-[1rem]">
+                  <Label className="text-[#D9D9D9]" htmlFor="email">
+                    Password*
+                  </Label>
+                  <Input
+                    required
+                    className="bg-[#D9D9D9] mt-[5px] rounded"
+                    type="password"
+                    name="password"
+                  />
+                </div>
+                <Button
+                  type="submit"
+                  className="py-[1.5rem] hover:bg-blue-600 font-bold text-[#ffffff] bg-[#1A88E1] w-full my-[6%] rounded"
+                >
+                  LOGIN IN
+                </Button>
+              </form>
               <p className="text-[#D9D9D9] text-center my-[0.5rem]">
                 Forgot Password?
               </p>
