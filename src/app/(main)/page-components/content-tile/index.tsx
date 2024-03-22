2299d865f55be633fd1a9733ed72f85119470ac4
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-22 22:00:20 +0530

    Request API integrated in the request page

--- src/app/(main)/page-components/content-tile/index.tsx
+++ src/app/(main)/page-components/content-tile/index.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import React, { useState } from 'react';
+import Image from "next/image";
+import React, { useState } from "react";
 
 export const ContentTile = ({ content }: { content: any }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -20,35 +20,43 @@ export const ContentTile = ({ content }: { content: any }) => {
       >
         <img
           className="rounded-[100%] w-[10%] h-[100%]"
-          src={content.imgPath}
+          src="/display-picture/display.png"
           alt="Project Logo"
         />
         <div className="ml-[1rem]">
-          <h3 className="text-[90%]">{content.title}</h3>
-          <p className="text-[60%] my-[1%]">{content.description}</p>
+          <h3 className="text-[90%]">{content.product}</h3>
+          <p className="text-[60%] my-[1%]">{content.product_desc}</p>
           <p className="text-[70%]">{content.date}</p>
         </div>
       </div>
       {isModalOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
           <div className="bg-white p-8 pb-4  w-[40%] h-[50%] flex flex-col items-start justify-around rounded-xl">
-            <div className='h-[30%] w-[20%] relative rounded-full'>
+            <div className="h-[30%] w-[20%] relative rounded-full">
               <Image
-                    src={content.imgPath}
-                    alt="User Image"
-                    fill={true}
-                    objectFit="fill"
-                    className="rounded-full"
-                  />
+                src="/display-picture/display.png"
+                alt="User Image"
+                fill={true}
+                objectFit="fill"
+                className="rounded-full"
+              />
             </div>
-            
-            <h2 className="text-[28px] font-bold">{content.title}</h2>
-            <p>{content.budget}</p>
-            <p className="mt-2">{content.description}</p>
+
+            <h2 className="text-[28px] font-bold">{content.product}</h2>
+            <p>{`₹${content.budget_max} - ₹${content.budget_min}`}</p>
+            <p className="">{content.product_desc}</p>
             <p>{content.date}</p>
             <div>
-              <button className="mt-4 bg-blue-500 text-white px-4  m-2 py-2 rounded" onClick={closeModal}>Accept Request</button>
-              <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded" onClick={closeModal}>
+              <button
+                className="mt-4 bg-blue-500 text-white px-4  m-2 py-2 rounded"
+                onClick={closeModal}
+              >
+                Accept Request
+              </button>
+              <button
+                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={closeModal}
+              >
                 Close
               </button>
             </div>
--- src/app/(main)/requests/page.tsx
+++ src/app/(main)/requests/page.tsx
@@ -4,125 +4,139 @@ import { Separator } from "@/components/ui/separator";
 import ContentTile from "../page-components/content-tile";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Requests = () => {
   const router = useRouter();
   if (!sessionStorage.getItem("userData")) {
     router.push("/login");
   }
-  const requests = [
-    {
-      imgPath: "/display-picture/display.png",
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-      budget: '200k-300k',
-    },
-    {
-      imgPath: "/display-picture/display.png",
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-    },
-    {
-      imgPath: "/display-picture/display.png",
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-    },
-    {
-      imgPath: "/display-picture/display.png",
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-    },
-    {
-        imgPath: "/display-picture/display.png",
-        title: "ABC Company",
-        description:
-          "Description about the company and the project it is offering ...",
-        date: "12 March 2024",
-      },
-      {
-        imgPath: "/display-picture/display.png",
-        title: "ABC Company",
-        description:
-          "Description about the company and the project it is offering ...",
-        date: "12 March 2024",
-        
-      },
-      {
-        imgPath: "/display-picture/display.png",
-        title: "ABC Company",
-        description:
-          "Description about the company and the project it is offering ...",
-        date: "12 March 2024",
-        
-      },
-      {
-        imgPath: "/display-picture/display.png",
-        title: "ABC Company",
-        description:
-          "Description about the company and the project it is offering ...",
-        date: "12 March 2024",
-        
-      },
-      {
-        imgPath: "/display-picture/display.png",
-        title: "ABC Company",
-        description:
-          "Description about the company and the project it is offering ...",
-        date: "12 March 2024",
-        
-      },
-      {
-        imgPath: "/display-picture/display.png",
-        title: "ABC Company",
-        description:
-          "Description about the company and the project it is offering ...",
-        date: "12 March 2024",
-        
-      },
-  ];
-  return (
-    <>
-      <div className="w-[80%]">
-        <div className="m-[1rem] ml-0 py-[1.8%] px-[2%] h-[96vh] rounded-xl bg-[#fff] ">
-          <div className="flex items-center justify-between mb-[0.3rem]">
-            <h2 className="text-[#717171] text-[28px] font-bold">Requests</h2>
-            <Image
-              className="cursor-pointer"
-              src={"/dashboard-vendor/expand.svg"}
-              width={20}
-              height={100}
-              alt="Project Logo"
-            ></Image>
-          </div>
-          <Separator className="bg-[#d7d7d7] mx-auto h-[0.2rem] text-center " />
-          <div className="flex flex-col h-[90%] w-[100%]">
-            <div className="flex flex-col flex-wrap w-[100%] h-[90%] m-0">
-              {requests.map((request) => {
-                return <ContentTile content={request} />;
-              })}
+
+  let userData: any = sessionStorage.getItem("userData");
+  userData = JSON.parse(userData);
+
+  // console.log(userData.user.email);
+
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["requirements-requests"],
+    queryFn: () =>
+      axios.get(`http://localhost:3000/requirements/${userData.user.email}`),
+  });
+
+  // console.log(data?.data?.response?.requirements);
+
+  // const requests = [
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //     budget: "200k-300k",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  //   {
+  //     imgPath: "/display-picture/display.png",
+  //     title: "ABC Company",
+  //     description:
+  //       "Description about the company and the project it is offering ...",
+  //     date: "12 March 2024",
+  //   },
+  // ];
+
+  if (data) {
+    return (
+      <>
+        <div className="w-[80%]">
+          <div className="m-[1rem] ml-0 py-[1.8%] px-[2%] h-[96vh] rounded-xl bg-[#fff] ">
+            <div className="flex items-center justify-between mb-[0.3rem]">
+              <h2 className="text-[#717171] text-[28px] font-bold">Requests</h2>
+              <Image
+                className="cursor-pointer"
+                src={"/dashboard-vendor/expand.svg"}
+                width={20}
+                height={100}
+                alt="Project Logo"
+              ></Image>
             </div>
-            <div className="h-[10%] flex items-center justify-center m-0">
-              <Button className="flex justify-center items-center">
-                <p className="font-bold text-gray-500">Load More</p>
-                <img
-                  src={"/dashboard-vendor/downarrow.svg"}
-                  className="w-4 h-4 ml-2"
-                ></img>
-              </Button>
+            <Separator className="bg-[#d7d7d7] mx-auto h-[0.2rem] text-center " />
+            <div className="flex flex-col h-[90%] w-[100%]">
+              <div className="flex flex-col flex-wrap w-[100%] h-[90%] m-0">
+                {data?.data?.response?.requirements.map((request: any) => {
+                  return <ContentTile content={request} />;
+                })}
+              </div>
+              <div className="h-[10%] flex items-center justify-center m-0">
+                <Button className="flex justify-center items-center">
+                  <p className="font-bold text-gray-500">Load More</p>
+                  <img
+                    src={"/dashboard-vendor/downarrow.svg"}
+                    className="w-4 h-4 ml-2"
+                  ></img>
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Requests;
