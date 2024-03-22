975b2632276840d873187d9ec8d6b1e5d8a83d42
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-22 22:35:39 +0530

    Request API integrated in the dashboard page

--- src/app/(main)/client_dashboard/page.tsx
+++ src/app/(main)/client_dashboard/page.tsx
@@ -6,12 +6,23 @@ import Requests from "../page-components/Requests";
 import ContentTile from "../page-components/content-tile";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Dashboard = () => {
   const router = useRouter();
   if (!sessionStorage.getItem("userData")) {
     router.push("/login");
   }
+
+  let userData: any = sessionStorage.getItem("userData");
+  userData = JSON.parse(userData);
+
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["requirements-requests"],
+    queryFn: () =>
+      axios.get(`http://localhost:3000/requirements/${userData.user.email}`),
+  });
 
   const contents = [
     {
@@ -43,47 +54,51 @@ const Dashboard = () => {
       date: "12 March 2024",
     },
   ];
-  return (
-    <>
-      <div className="w-[80%]">
-        <div className="m-[1rem] ml-0 py-[1.8%] px-[2%] h-[48vh] rounded-xl bg-[#fff] ">
-          <div className="flex items-center justify-between mb-[0.3rem]">
-            <h2 className="text-[#717171] text-[28px] font-bold">Dashboard</h2>
-            <Image
-              onClick={() => router.push("/post_requirement")}
-              className="cursor-pointer"
-              src={"/dashboard-vendor/add.svg"}
-              width={35}
-              height={100}
-              alt="Project Logo"
-            ></Image>
-          </div>
-          <Separator className="bg-[#d7d7d7] mx-auto h-[0.2rem] text-center " />
-          <div className="flex flex-col h-[90%] w-[100%]">
-            <div className="flex flex-col flex-wrap w-[100%] h-[90%] m-0">
-              {contents.map((content) => {
-                return <ContentTile content={content} />;
-              })}
+  if (data) {
+    return (
+      <>
+        <div className="w-[80%]">
+          <div className="m-[1rem] ml-0 py-[1.8%] px-[2%] h-[48vh] rounded-xl bg-[#fff] ">
+            <div className="flex items-center justify-between mb-[0.3rem]">
+              <h2 className="text-[#717171] text-[28px] font-bold">
+                Dashboard
+              </h2>
+              <Image
+                onClick={() => router.push("/post_requirement")}
+                className="cursor-pointer"
+                src={"/dashboard-vendor/add.svg"}
+                width={35}
+                height={100}
+                alt="Project Logo"
+              ></Image>
             </div>
-            <div className="h-[10%] flex items-center justify-center m-0">
-              <Button className="flex justify-center items-center">
-                <p className="font-bold text-gray-500">Load More</p>
-                <img
-                  src={"/dashboard-vendor/downarrow.svg"}
-                  className="w-4 h-4 ml-2"
-                ></img>
-              </Button>
+            <Separator className="bg-[#d7d7d7] mx-auto h-[0.2rem] text-center " />
+            <div className="flex flex-col h-[90%] w-[100%]">
+              <div className="flex flex-col flex-wrap w-[100%] h-[90%] m-0">
+                {data?.data?.response?.requirements.map((content: any) => {
+                  return <ContentTile content={content} />;
+                })}
+              </div>
+              <div className="h-[10%] flex items-center justify-center m-0">
+                <Button className="flex justify-center items-center">
+                  <p className="font-bold text-gray-500">Load More</p>
+                  <img
+                    src={"/dashboard-vendor/downarrow.svg"}
+                    className="w-4 h-4 ml-2"
+                  ></img>
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex">
-          {/* <Messages /> */}
-          <Requests />
+          <div className="flex">
+            {/* <Messages /> */}
+            <Requests />
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Dashboard;
