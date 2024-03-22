10ba9438c25b0edb86394548bd7dcad08fee56f5
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-03-22 20:16:28 +0530

    Requirement API integrated in the requirement page

--- src/components/requirement.tsx
+++ src/components/requirement.tsx
@@ -3,8 +3,21 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 const Requirement: React.FC = () => {
+  let user: any = sessionStorage.getItem("userData");
+  user = JSON.parse(user);
+  const { mutate, isError, isSuccess, isPending, data, error }: any =
+    useMutation({
+      mutationFn: (requirement) => {
+        return axios
+          .post("http://localhost:3000/requirements/post", requirement)
+          .then((res) => res.data);
+      },
+    });
+
   const router = useRouter();
   return (
     <div className="w-[80%] m-auto flex mb-4 flex-col h-full items-center">
@@ -16,99 +29,78 @@ const Requirement: React.FC = () => {
 
       {/* Form Section */}
       <div className="w-[65%] mx-auto my-[3%]">
-        {/* <div className="flex flex-row mt-[4%]">
-          <div className="mr-auto w-[45%]">
-            <Label className="text-[#D9D9D9]" htmlFor="company">
-              Company
+        <form
+          onSubmit={(event: any) => {
+            event.preventDefault();
+            mutate({
+              email: user.user.email,
+              product: event.target.product.value,
+              product_desc: event.target.description.value,
+              budget_min: event.target.min_budget.value,
+              budget_max: event.target.max_budget.value,
+            });
+
+            // console.log(user.user.email);
+            // console.log(event.target.product.value);
+            // console.log(event.target.description.value);
+            // console.log(event.target.min_budget.value);
+            // console.log(event.target.max_budget.value);
+          }}
+        >
+          <div className="flex flex-col mx-auto mt-[6%]">
+            <Label className="text-[#D9D9D9]" htmlFor="requirement">
+              Required service/product
             </Label>
-            <Input
-              id="company"
-              className="bg-[#D9D9D9] mt-[5px] rounded"
-              type="text"
-            />
-          </div>
-          <div className="w-[45%]">
-            <Label className="text-[#D9D9D9]" htmlFor="title">
-              Title
+            <textarea
+              name="product"
+              id="requirement"
+              className="bg-[#D9D9D9] mt-[10px] rounded w-full h-[60px] p-2 resize-none"
+            ></textarea>
+            <Label className="text-[#D9D9D9] mt-[5%]" htmlFor="text">
+              Description
             </Label>
-            <Input
-              id="title"
-              className="bg-[#D9D9D9] mt-[5px] rounded"
-              type="text"
-            />
+            <textarea
+              name="description"
+              id="description"
+              className="bg-[#D9D9D9] mt-[10px] rounded w-full p-2 resize-none"
+              rows={3}
+            ></textarea>
           </div>
-        </div> */}
-        {/* <div className="flex flex-row mt-[4%]">
-          <div className="mr-auto w-[45%]">
-            <Label className="text-[#D9D9D9]" htmlFor="price">
-              Sector
-            </Label>
-            <Input
-              id="price"
-              className="bg-[#D9D9D9] mt-[5px] rounded"
-              type="number"
-            />
-          </div>
-          <div className="w-[45%]">
-            <Label className="text-[#D9D9D9]" htmlFor="location">
-              Location
-            </Label>
-            <Input
-              id="location"
-              className="bg-[#D9D9D9] mt-[5px] rounded"
-              type="text"
-            />
-          </div>
-        </div> */}
-        <div className="flex flex-col mx-auto mt-[6%]">
-          <Label className="text-[#D9D9D9]" htmlFor="requirement">
-            Required service/product
-          </Label>
-          <textarea
-            id="requirement"
-            className="bg-[#D9D9D9] mt-[10px] rounded w-full h-[60px] p-2 resize-none"
-          ></textarea>
-          <Label className="text-[#D9D9D9] mt-[5%]" htmlFor="text">
-            Description
-          </Label>
-          <textarea
-            id="description"
-            className="bg-[#D9D9D9] mt-[10px] rounded w-full p-2 resize-none"
-            rows={3}
-          ></textarea>
-        </div>
-        <div className="flex flex-col mt-[4%] w-[100%]">
-          <Label className="text-[#D9D9D9]">Budget</Label>
-          <div className="flex flex-row items-center">
-            <div className="mr-auto w-[40%] mt-2 flex flex-col">
-              <Input
-                placeholder=" Min."
-                id="minPrice"
-                className="bg-[#D9D9D9] mt-[5px] rounded"
-                type="number"
-              />
-            </div>
-            <div className="w-[40%] flex flex-col">
-              <Input
-                placeholder=" Max."
-                id="maxPrice"
-                className="bg-[#D9D9D9] mt-[5px] rounded"
-                type="number"
-              />
+          <div className="flex flex-col mt-[4%] w-[100%]">
+            <Label className="text-[#D9D9D9]">Budget</Label>
+            <div className="flex flex-row items-center">
+              <div className="mr-auto w-[40%] mt-2 flex flex-col">
+                <Input
+                  name="min_budget"
+                  placeholder=" Min."
+                  id="minPrice"
+                  className="bg-[#D9D9D9] mt-[5px] rounded"
+                  type="number"
+                />
+              </div>
+              <div className="w-[40%] flex flex-col">
+                <Input
+                  name="max_budget"
+                  placeholder=" Max."
+                  id="maxPrice"
+                  className="bg-[#D9D9D9] mt-[5px] rounded"
+                  type="number"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col mx-auto mt-[1rem]">
-          <Button className="my-[1rem] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Submit
-          </Button>
-          <Button
-            onClick={() => router.push("/client_dashboard")}
-            className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Back
-          </Button>
-        </div>
+          <div className="flex flex-col mx-auto mt-[1rem]">
+            <Button className="my-[1rem] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Submit
+            </Button>
+            <Button
+              onClick={() => router.push("/client_dashboard")}
+              className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Back
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
